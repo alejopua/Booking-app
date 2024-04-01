3260f5d6474efe422ed44bcbb8eea85dd0c5f1d1
alejopua <alejopua@gmail.com>
2024-04-01 13:56:44 -0500

    Add Toaster component for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Route, Switch } from 'wouter'
+import { Toaster } from 'react-hot-toast'
 import { HotelList } from './components/HotelList'
 import { HotelDetails } from './components/HotelDetails'
 
@@ -8,11 +9,17 @@ const client = new QueryClient()
 export const App = () => {
 
   return (
-    <QueryClientProvider client={client}>
-      <Switch>
-        <Route path='/' component={HotelList}/>
-        <Route path='/hotel/:id' component={HotelDetails}/>
-      </Switch>
-    </QueryClientProvider>
+    <>
+    <Toaster
+      position="top-left"
+      reverseOrder={false}
+    />
+      <QueryClientProvider client={client}>
+        <Switch>
+          <Route path='/' component={HotelList}/>
+          <Route path='/hotel/:id' component={HotelDetails}/>
+        </Switch>
+      </QueryClientProvider>
+    </>
   )
 }
