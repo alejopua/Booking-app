a615b584f8f242b611c9a51966890f95fdbb4120
alejopua <alejopua@gmail.com>
2024-04-01 13:04:48 -0500

    Add react-query and routing to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Route, Switch } from 'wouter'
+import { HotelList } from './components/HotelList'
+import { HotelDetails } from './components/HotelDetails'
+
+const client = new QueryClient()
 
 export const App = () => {
+
   return (
-    <div>App</div>
+    <QueryClientProvider client={client}>
+      <Switch>
+        <Route path='/' component={HotelList}/>
+        <Route path='/hotel/:id' component={HotelDetails}/>
+      </Switch>
+    </QueryClientProvider>
   )
 }
