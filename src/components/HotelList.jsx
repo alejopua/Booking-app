f1d6c7b918d90a7e406487371dc724ae87bc735e
alejopua <alejopua@gmail.com>
2024-04-01 13:04:38 -0500

    Refactor HotelList component and update API endpoint

--- src/components/HotelList.jsx
+++ src/components/HotelList.jsx
@@ -1,16 +1,16 @@
 import { useQuery } from '@tanstack/react-query'
+import { Typography, Stack, Card, CardMedia, CardContent, CardActions, Button, Box } from '@mui/material'
+import { Link } from 'wouter'
 
 const fetchHotels = async () => {
-  fetch('http://localhost:3000/hotels')
-    .then( res => {
-      if ( !res.ok ) {
-        throw new Error('Network response was not ok')
-      }
-      return res.json()
-    })
+  const res = await fetch('http://localhost:3001/hotels')
+    if ( !res.ok ) {
+      throw new Error('Network response was not ok')
+    }
+    return res.json()
 }
 
-const hotelList = () => {
+export const HotelList = () => {
   const { data: hotels, isLoading, error } = useQuery({ queryKey: ['hotels'], queryFn: fetchHotels })
 
   if ( isLoading ) {
@@ -19,4 +19,31 @@ const hotelList = () => {
   if ( error ) {
     return <div>Error: {error.message}</div>
   }
+
+  return (
+    <>
+      <Box display={'flex'} flexDirection={'column'} flexWrap={'wrap'} alignItems={'center'}>
+        <Typography variant="h4" component='h2'>Booking App</Typography>
+        <Stack spacing={2}>
+          {hotels.map( hotel => (
+            <Link style={{ textDecoration: 'none' }} key={hotel.id} href={`/hotel/${hotel.id}`}>
+              <Card sx={{ maxWidth: 345, backgroundColor: '#e8e8e1'}}>
+                <CardMedia sx={{ height: 140 }} title={hotel.name} image={hotel.image} />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">{hotel.name}</Typography>
+                  <Typography variant="body2" color="text.secondary">{hotel.description}</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size='small'>
+                    See Details
+                  </Button>
+                </CardActions>
+              </Card>
+            </Link>
+          ))}
+        </Stack>
+      </Box>
+    
+    </>
+  )
 }
